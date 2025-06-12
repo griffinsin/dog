cc4a887330efe34763c1cc72f948e296debd29c8
griffin <griffin@gmail.com>
2025-06-12 15:09:44 +0900

    更新版本号到 1.0.39 并包含其他修改

--- bin/commands/hello.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Load global variables and functions
-source $(dirname "$(dirname "$(dirname "${BASH_SOURCE[0]}")")")/lib/globals.sh
-
-# hello command implementation
-dog_log "Hello, World!"
-dog_success "hello command executed successfully"
--- lib/globals.sh
+++ lib/globals.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 全局变量
-DOG_VERSION="1.0.38"
+DOG_VERSION="1.0.39"
 
 # 全局函数
 dog_log() {
