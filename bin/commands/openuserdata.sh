564da9e64350170e8a428b5a700b2a4b8912e78d
griffin <griffin@gmail.com>
2025-06-12 11:49:54 +0900

    添加 openuserdata 命令用于打开 Xcode UserData 文件夹

---
+++ bin/commands/openuserdata.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Load global variables and functions
+source $(dirname "$(dirname "$(dirname "${BASH_SOURCE[0]}")")")/lib/globals.sh
+
+# Open Xcode UserData folder command implementation
+dog_log "正在打开 Xcode UserData 文件夹..."
+
+# Open Xcode UserData folder
+open ~/Library/Developer/Xcode/UserData
+
+dog_success "Xcode UserData 文件夹已打开"
