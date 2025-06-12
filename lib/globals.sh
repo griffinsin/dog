1da74dfde689a6e5725439cc8614fbd92b1d1d36
griffin <griffin@gmail.com>
2025-06-12 13:13:07 +0900

    更新版本号到 1.0.36 并包含其他修改

--- lib/globals.sh
+++ lib/globals.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 全局变量
-DOG_VERSION="1.0.35"
+DOG_VERSION="1.0.36"
 
 # 全局函数
 dog_log() {
@@ -14,4 +14,12 @@ dog_error() {
 
 dog_success() {
   echo "[dog 成功] $1"
+}
+
+jetbrains_apps=("PyCharm" "GoLand" "IntelliJ IDEA" "IntelliJ IDEA CE" "WebStorm" "CLion" "PhpStorm" "RubyMine" "DataGrip")
+
+print_color() {
+	local color_code="$1"
+	local text="$2"
+	printf "\033[${color_code}m${text}\033[0m"
 }
