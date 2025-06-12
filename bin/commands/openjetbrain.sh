c43ccc4c3e07d43448cf649d4863a9ed81d5ab84
griffin <griffin@gmail.com>
2025-06-12 11:52:19 +0900

    添加 openjetbrain 命令用于打开 JetBrains 配置文件夹

---
+++ bin/commands/openjetbrain.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Load global variables and functions
+source $(dirname "$(dirname "$(dirname "${BASH_SOURCE[0]}")")")/lib/globals.sh
+
+# Open JetBrains folder command implementation
+dog_log "正在打开 JetBrains 配置文件夹..."
+
+# Open JetBrains folder
+open ~/Library/Application\ Support/JetBrains
+
+dog_success "JetBrains 配置文件夹已打开"
