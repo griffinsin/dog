90da51ce69db2b3fadca5165dd8583e88dd2970b
griffin <griffin@gmail.com>
2025-06-13 11:45:18 +0900

    添加 img 命令用于显示 ImageMagick 的安装和使用指南

---
+++ bin/commands/img.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# 加载全局变量和函数
+source $(dirname "$(dirname "$(dirname "${BASH_SOURCE[0]}")")")/lib/globals.sh
+
+# img 命令实现
+dog_log "ImageMagick 使用指南"
+echo ""
+
+echo "安装 ImageMagick:"
+print_color "$BLUE" "brew install imagemagick"
+echo ""
+echo ""
+
+echo "压缩图片质量:"
+print_color "$BLUE" "magick input.jpg -quality 85 output.jpg"
+echo ""
+echo ""
+
+echo "调整图片大小:"
+print_color "$BLUE" "magick input.png -resize 50% output.png"
+echo ""
+
+dog_success "ImageMagick 使用指南显示完成"
